a729cd10f3483878ec435f9304b4022e35824fa0
Vishal Binit <vishalbinit211@gmail.com>
2026-03-08 18:29:18 +0530

    Added docs navigation test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,8 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('open docs page', async ({ page }) => {
+  await page.goto('https://playwright.dev/docs/intro');
+  await expect(page).toHaveURL(/docs/);
+});
